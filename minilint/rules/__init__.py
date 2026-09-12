7d77f571c131fde86b5d1b33065d719bb6db379f
nithyashreeravichandran354-arch <nithyashreeravichandran354@gmail.com>
2026-09-12 15:00:15 +0530

    Update __init__.py

--- minilint/rules/__init__.py
+++ minilint/rules/__init__.py
@@ -1,9 +1,13 @@
+
 from .bare_except import BareExceptRule
 from .none_comparison import NoneComparisonRule
 from .dead_code import DeadCodeRule
 from .unused_variable import UnusedVariableRule
 from .shadowed_variable import ShadowedVariableRule
 from .use_before_assignment import UseBeforeAssignmentRule
+from .mutable_default_argument import MutableDefaultArgumentRule
+from .too_many_arguments import TooManyArgumentsRule
+from .unused_import import UnusedImportRule
 
 ALL_RULES = [
     BareExceptRule,
@@ -12,6 +16,9 @@ ALL_RULES = [
     UnusedVariableRule,
     ShadowedVariableRule,
     UseBeforeAssignmentRule,
+    MutableDefaultArgumentRule,
+    TooManyArgumentsRule,
+    UnusedImportRule,
 ]
 
 __all__ = ["ALL_RULES"] + [cls.__name__ for cls in ALL_RULES]
